85aed5eda0cb4d49266cf3e031707f86fb575756
SPRHackz <sprhackz003@gmail.com>
2023-09-05 20:14:03 +0530

    To Add Breed Name for Each Dog Image Under

--- src/Components/Dog.jsx
+++ src/Components/Dog.jsx
@@ -1,29 +1,29 @@
-import  { useState, useEffect } from 'react';
-import BreedSelector from './BreedSelector';
-import DogImage from './DogImage';
-import Header from './Header';
-import Footer from './Footer';
+import { useState, useEffect } from "react";
+import BreedSelector from "./BreedSelector";
+import DogImage from "./DogImage";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 function Dog() {
   const [imgLinks, setImgLinks] = useState([]);
   const [breeds, setBreeds] = useState([]);
-  const [selectedBreed, setSelectedBreed] = useState('');
+  const [selectedBreed, setSelectedBreed] = useState("");
   const [error, setError] = useState(null);
 
   const getBreeds = () => {
-
-    fetch('https://dog.ceo/api/breeds/list/all')
+    fetch("https://dog.ceo/api/breeds/list/all")
       .then((response) => response.json())
       .then((data) => {
-        if (data.status === 'success') {
+        if (data.status === "success") {
           const breedList = Object.keys(data.message);
           setBreeds(breedList);
         } else {
-          setError('Select The Breed Of The Dog');
+          setError("Select The Breed Of The Dog");
         }
       })
       .catch(() => {
-        setError('Failed to fetch breeds');
+        setError("Failed to fetch breeds");
       });
   };
   const getRandomDog = () => {
@@ -31,46 +31,49 @@ function Dog() {
       .then((response) => response.json())
       .then((dog) => {
         if (dog.status === 'success') {
-          setImgLinks([dog.message]); 
+          const breedName = dog.message.split('/')[4];
+          setImgLinks([{ url: dog.message, breed: breedName }]);
           setError(null);
         } else {
-          setError('Select The Breed Of The Dog');
+          setError('Failed to fetch image');
         }
       })
       .catch(() => {
         setError('Failed to fetch image');
       });
   };
+  
   const getDogs = () => {
-    if(selectedBreed !==""){
+    if (selectedBreed !== "") {
       fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random`)
-    .then((response) => response.json())
-    .then((dog) => {
-      if (dog.status === 'success') {
-        setImgLinks((prevLinks) => [dog.message, ...prevLinks]);
-        setError(null);
-      } else {
-        setError('Select The Breed Of The Dog');
-      }
-    })
-    .catch(() => {
-      
-      setError('Failed to Fetch Image');
-    });
-    }else{
-      getRandomDog()
+        .then((response) => response.json())
+        .then((dog) => {
+          if (dog.status === "success") {
+            setImgLinks((prevLinks) => [
+              { url: dog.message, breed: selectedBreed },
+              ...prevLinks,
+            ]);
+            setError(null);
+          } else {
+            setError("Failed to fetch image");
+          }
+        })
+        .catch(() => {
+          setError("Failed to fetch image");
+        });
+    } else {
+      getRandomDog();
     }
   };
-
   useEffect(() => {
     getBreeds();
     getRandomDog();
-    console.log('Component mounted');
+    console.log("Component mounted");
   }, []);
 
   return (
-    <div className='dog-box'>
-    <Header/>      
+    <div className="dog-box">
+      <Header />
       <DogImage imgLinks={imgLinks} error={error} />
       <BreedSelector
         breeds={breeds}
@@ -78,9 +81,9 @@ function Dog() {
         setSelectedBreed={setSelectedBreed}
         getDogs={getDogs}
       />
-     <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default Dog;
